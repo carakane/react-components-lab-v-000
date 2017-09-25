98230321dea4e8d8532421616b6d4a3a99184318
Cara Kane <cara.kane@gmail.com>
2017-09-25 16:15:43 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -12,6 +12,13 @@ class OlderCoaster extends React.Component{
   }
 }
 
+class InFrontOfYou extends React.Component{
+  render(){
+    return React.createElement('div', {},
+      React.createElement('p', {}, 'You shouldn\'t look too far.'),
+      React.createElement('p', {}, 'Sometimes, the solution is right in front of you.'))  
+  }
+}
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
